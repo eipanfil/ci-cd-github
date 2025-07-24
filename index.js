3b9486fb673616abbbd6ad4bf231cc0694735fd2
Evgenij Panfilov <kinloguert@yandex-team.ru>
2025-07-25 03:41:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ exports.handler = async function (event, context) {
 
   // Проверяем наличие "hello" без учёта регистра
   if (inputStr.toLowerCase().includes('hello')) {
-      result = "Hello, customzzz 134217728!";
+      result = "Hello, preprod with memory!";
   } else {
       result = "Nothing to say!";
   }
